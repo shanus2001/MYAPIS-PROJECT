f1341403cdae21ff99b9a0902564cf184f64e4b5
shanu <shanuchoudharyvikas@gmail.com>
2024-03-14 12:40:42 +0530

    apis1

---
+++ APIS/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+# Create your views here.
+@api_view(["GET"])
+def myhome(request):
+    return Response({
+        "Message":"this is myn first apis"
+    })
